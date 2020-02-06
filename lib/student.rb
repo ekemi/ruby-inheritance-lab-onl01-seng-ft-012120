769f5c7ec861b36aab4845f570af8bf516fed568
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-06 05:26:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+class Student< User
 
+   def initialize(knowledge=[])
+     
+     @knowledge = knowledge
+   end
+     
+     def learn(string_knowledge)
+       
+       @knowledge << string_knowledge
+       
+     end
+     
+     
+     def knowledge
+       
+      @knowledge 
+       
+     end
+     
 end
